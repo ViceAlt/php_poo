780f227e8c3ed59b1d56a3a49d2b3a5d3f6f5387
Vito Virito <vitor.landin@aluno.senai.br>
2026-03-11 08:09:12 -0300

    Submit final

---
+++ exemplo03.php
@@ -0,0 +1,20 @@
+<?php
+
+function calcularDescontos($salario, $previdencia){
+    return number_format($salario*0.275 + $previdencia, 2, ',', '.');
+
+}
+
+$funcionarios = array(3);
+$funcionarios[0] = array('João Filho', 1000, 100);
+$funcionarios[1] = array('Maria Rute', 2000, 200);
+$funcionarios[2] = array('José Salgado', 3000, 300);
+
+//o i começa como zero, é menor que 3 e se auto-incrimenta, funcionando como uma planilha.
+
+for ($i = 0; $i < 3; $i++){
+    $nome = $funcionarios[$i][0];
+    $descontos = calcularDescontos($funcionarios[$i][1], $funcionarios[$i][2]);
+
+    echo "O Valor do desconto de $nome é de R$ $descontos. <br>";
+}
